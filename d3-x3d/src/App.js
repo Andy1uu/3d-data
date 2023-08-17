e8f36bd480d66c2d1d8c89f25fa050880021146e
Andy Luu <andyluu324@gmail.com>
2023-08-17 11:33:43 -0400

    Ran into babel issues

---
+++ d3-x3d/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TestChart from './TestChart';
+
+function App() {
+  return (
+    <div className="App">
+      <TestChart />
+    </div>
+  );
+}
+
+export default App;
---
+++ d3-x3d/src/TestChart.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from "react";
+import * as d3_x3d from "d3-x3d";
+
+const TestChart = () => {
+
+}
+export default TestChart;
